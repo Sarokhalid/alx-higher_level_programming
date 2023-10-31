f6106baa163899d56738925b7d68f3a68568f4fc
sara <saramustafa1842002@gmail.com>
2023-10-31 05:07:19 -0700

    task done

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(10):
-    for q in range(10):
-        print("{}{}".format(i,q), end=", " if q < 9 else "\n")
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=", ")
+print()
