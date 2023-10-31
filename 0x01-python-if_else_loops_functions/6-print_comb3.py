9de2f92d279745c10bc4dedf57482f9d5f77fcfd
sara <saramustafa1842002@gmail.com>
2023-10-31 05:19:43 -0700

    task done

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end=", ")
+        if i == 0:
+            print("{:d}{:d}".format(i, j), end=", ")
+        else:
+            print("{:02d}".format(int(str(i) + str(j))), end=", ")
 print()
