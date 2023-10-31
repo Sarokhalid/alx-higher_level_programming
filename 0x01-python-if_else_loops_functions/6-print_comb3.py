5620233e122af7f73b18eca9415517995e5b8ae9
sara <saramustafa1842002@gmail.com>
2023-10-31 10:13:48 -0700

    task done

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:02d}".format(int(str(i) + str(j))), end=", ", if i == 8 and j == 9 "\n")
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d}".format(i, j), end=", ")
+print()
