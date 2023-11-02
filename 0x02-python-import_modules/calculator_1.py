d8e4ddee1f6f5c9fd3bbabebe1aec491c00999d8
sara <saramustafa1842002@gmail.com>
2023-11-02 09:25:03 -0700

    task done

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -9,37 +9,33 @@ def add(a, b):
     """	
     return (a + b)
 def sub(a, b):
-		    """My subtraction function
+    """My subtraction function
 
-			        Args:
-				        a: first integer
-					        b: second integer
-
-						    Returns:
-						            The return value. a - b
-							        """
+    Args:
+        a: first integer
+        b: second integer
+	Returns:
+	The return value. a - b
+    """
     return (a - b)
 
 
 def mul(a, b):
-		    """My multiplication function
-
-			        Args:
-				        a: first integer
-					        b: second integer
-
-						    Returns:
-						            The return value. a * b
-							        """
+    """My multiplication function
+    Args:
+    a: first integer
+    b: second integer
+    Returns:
+    The return value. a * b
+    """
     return (a * b)
 def div(a, b):
-		    """My division function
+    """My division function
+    Args:
+        a: first integer
+        b: second integer
 
-			        Args:
-				        a: first integer
-					        b: second integer
-
-						    Returns:
-						            The return value. a / b
-							        """
+    Returns:
+        The return value. a / b
+    """
     return int(a / b)
