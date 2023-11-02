dec18632264316808b294e0656bf5892c6200e6d
sara <saramustafa1842002@gmail.com>
2023-11-02 10:46:00 -0700

    task done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
-args = sys.argv[1:]
-num_args = len(args)
-arg_str = "argument" if num_args == 1 else "arguments"
-print(f"{num_args} {arg_str}:", end="")
-print("." if num_args == 0 else "")
-for i, arg in enumerate(args):
-    print(f"{i+1}: {arg}")
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    num_args = len(args)
+    arg_str = "argument" if num_args == 1 else "arguments"
+    print(f"{num_args} {arg_str}:", end="")
+    print("." if num_args == 0 else "")
+    for i, arg in enumerate(args):
+        print(f"{i+1}: {arg}")
