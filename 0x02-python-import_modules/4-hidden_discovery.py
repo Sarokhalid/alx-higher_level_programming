7c38434527cc7ebd50093abdd8026c5e6e8e7cd5
sara <saramustafa1842002@gmail.com>
2023-11-02 11:35:56 -0700

    task done

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
-import dis
 if __name__ == "__main__":
-    with open("hidden_4.pyc", "rb") as file:
-        bytecode = file.read()
-        instructions = dis.get_instructions(bytecode)
-        names = set()
-        for instr in instructions:
-            if instr.opname == "LOAD_CONST":
-                const = instr.argval
-                if isinstance(const, str) and not const.startswith("__"):
-                    names.add(const)
-        for name in sorted(names):
-            print(name)
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
