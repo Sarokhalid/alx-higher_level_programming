f67632556662788ce5b57ce2515dff3c7bba54a1
sara <saramustafa1842002@gmail.com>
2023-11-21 16:24:48 -0800

    done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""square module"""
+
+class square:
+    def __init__(self, size):
+        """
+        Args:
+            size: lenght of a side of the square
+	"""
+        self.__size = size
