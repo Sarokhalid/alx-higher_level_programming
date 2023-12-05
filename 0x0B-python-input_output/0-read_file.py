cf977957d4083965ed82915e01be0e3b4b95475f
sara <saramustafa1842002@gmail.com>
2023-12-05 06:29:32 -0800

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""class"""
+
+
 def read_file(filename=""):
+    """read file"""
     with open(filename, mode='r', encoding='utf-8') as file:
         for line in file:
             print(line, end='')
