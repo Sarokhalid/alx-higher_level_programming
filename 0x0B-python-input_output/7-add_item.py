1e9221da4e2baa2706acd45a037c5a55fe2587d6
sara <saramustafa1842002@gmail.com>
2023-12-05 07:54:19 -0800

    done

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python3
-"""fhjjj"""
+"""fhkk"""
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+from typing import List
 
-try:
-    existing_data = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    existing_data = []
+
+def save_to_json_file(my_obj, filename):
+    """dhkk"""
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
+
+
+def load_from_json_file(filename):
+    """dhkk"""
+    with open(filename, "r") as file:
+        return json.load(file)
+
+
+def add_arguments_to_list(arguments: List[str]):
+    """cdhkkl"""
+    try:
+        existing_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        existing_list = []
+    existing_list.extend(arguments)
+    save_to_json_file(existing_list, "add_item.json")
+
+
 arguments = sys.argv[1:]
-data = existing_data + arguments
-save_to_json_file(data, "add_item.json")
+add_arguments_to_list(arguments)
