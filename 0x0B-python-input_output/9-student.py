107255fb2794b207ec0f2577b765ee1df91a2985
sara <saramustafa1842002@gmail.com>
2023-12-05 08:40:04 -0800

    done

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -14,7 +14,7 @@ class Student:
         """fhkk"""
         json_dict = {
             "first_name": self.first_name,
-            "last_name": self.last_name
+            "last_name": self.last_name,
             "age": self.age
         }
         return json_dict
