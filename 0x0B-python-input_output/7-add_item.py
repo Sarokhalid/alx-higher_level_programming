94ba5c4614a1d83de1344792a241c1100845da18
sara <saramustafa1842002@gmail.com>
2023-12-05 08:43:09 -0800

    done

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """hdjohej"""
-
-
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -10,6 +8,5 @@ try:
     old_data = load_from_json_file('add_item.json')
 except Exception:
     old_data = []
-
 old_data.extend(arglist)
 save_to_json_file(old_data, 'add_item.json')
