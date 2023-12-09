e516c43450bed23bfb517f507c7d5caf59f244a5
sara <saramustafa1842002@gmail.com>
2023-12-09 07:37:38 -0800

    all done

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -9,7 +9,7 @@ def compute_metrics():
     """fjkk"""
     total_size = 0
     status_counts = {200: 0, 301: 0, 400: 0, 401: 0,
-                       403: 0, 404: 0, 405: 0, 500: 0}
+                   403: 0, 404: 0, 405: 0, 500: 0}
     try:
         line_count = 0
         for line in sys.stdin:
